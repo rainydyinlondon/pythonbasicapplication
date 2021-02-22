b589c2ba31f9e8cc0c09d20f2eda33927b84d988
rainydyinlondon <79206366+rainydyinlondon@users.noreply.github.com>
2021-02-22 17:17:51 +0300

    Add files via upload

---
+++ Iterators.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 15:15:04 2021
+
+@author: Feyza
+"""
+
+#Iterable
+
+
+
+#Iterator
+
+
+
+
+sayilar=[1,2,3,4,5]      # 
+iterator=iter(sayilar)   # 
+
+
+
+while True:
+    try:
+        sayi=next(iterator)
+        print(sayi)
+    except StopAsyncIteration:
+        break
+
+
+# a=10
+# for i in sayilar:
+#     print(i)
+    
+# print(dir(sayilar))
+
+
+
+
+
+# iterator=iter(sayilar)
+
+# print(iterator)
+
+
+# print(next(sayilar))
